c248c0de5b3840ddba125c3a276e9b2d23fda5c9
Navdeep Gill <navdeep@h2o.ai>
2016-07-01 09:50:25 -0700

    Modify print statement

--- h2o-py/tests/testdir_munging/slice/pyunit_concat.py
+++ h2o-py/tests/testdir_munging/slice/pyunit_concat.py
@@ -7,14 +7,14 @@ def concat():
     df1 = h2o.create_frame(integer_fraction=1,binary_fraction=0,categorical_fraction=0,seed=1)
     df2 = h2o.create_frame(integer_fraction=1,binary_fraction=0,categorical_fraction=0,seed=2)
     df3 = h2o.create_frame(integer_fraction=1,binary_fraction=0,categorical_fraction=0,seed=3)
-    print df1
-    print df2
-    print df3
+    print(df1)
+    print(df2)
+    print(df3)
 
     df123 = df1.concat([df1,df2,df3])
     rows, cols = df123.dim
-    print rows,cols
-    print df123
+    print(rows,cols)
+    print(df123)
     assert rows == 10000 and cols == 30, "unexpected dimensions in original"
 
 if __name__ == "__main__":
